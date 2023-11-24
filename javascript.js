7ce5e80f77f04414fcbcd8751927060dc7c4eed5
gutochiele <63303276+gutochiele@users.noreply.github.com>
2023-11-24 20:17:28 +0100

    created forloop to create all the divs

---
+++ javascript.js
@@ -0,0 +1,7 @@
+
+    for(x=0; x<256;x++) {
+        const divBoard = document.createElement('div');
+        divBoard.className = "grid";
+        divBoard.innerText = 'o'
+    document.getElementById('board').appendChild(divBoard);
+    }
