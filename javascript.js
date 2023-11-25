1831583ab2c913dfd9f6c7a49f1f27884756ecdc
gutochiele <63303276+gutochiele@users.noreply.github.com>
2023-11-25 16:27:59 +0100

    made it with change grid and reset buttons

--- javascript.js
+++ javascript.js
@@ -1,8 +1,16 @@
+  
+let gridX = 16;
+let gridY = 16;
 
-    for(y = 0; y < 16; y++){
-    for(x = 0; x < 16; x++){
-        var divBoard = document.createElement('div');
+function startPad(gridX, gridY) {
+  board.innerHTML = '';
+
+    for(let y = 0; y < gridY; y++){
+      for(let x = 0; x < gridX; x++){
+        const divBoard = document.createElement('div');
         divBoard.className = 'grid';
+        divBoard.style.flex = `1 1 calc(100% / ${gridY})`;
+        divBoard.style.height = `calc(100% / ${gridX})`;
         document.getElementById('board').appendChild(divBoard);
     }
 }
@@ -14,3 +22,21 @@ grid.forEach((gridItem) => {
     event.target.style.backgroundColor = 'black';
   });
 });
+
+
+}
+startPad(gridX, gridY)
+
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', () => {
+  const userInput = prompt('How many elements per line in your grid? (max 100)');
+  if (userInput !== null && userInput !== '' && userInput <= 100) {
+    gridX = gridY = userInput;
+    startPad(gridX, gridY);
+}
+})
+
+const rstBtn = document.querySelector('.rstBtn')
+rstBtn.addEventListener('click', () => {
+  startPad(gridX, gridY)});
